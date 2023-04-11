cd559ad71237ce8eabd43fbbb6a63904a6a361dd
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-04-11 15:47:45 +0530

    create sticky navbar

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
-
-let menuicons = document.getElementById('navbaricons');
+let menuicons = document.getElementById('navbaricon');
 menuicons.addEventListener('click' , function(){
     document.getElementById('navbar').style.transition= '0.5s'
     document.getElementById('navbar').style.visibility = 'visible'
 })
 let crossicons = document.getElementById('crossicon');
 crossicons.addEventListener('click', function(){
-  document.getElementById('navbar').style.visibility = 'hidden';
+    document.getElementById('navbar').style.visibility = 'hidden'
+    document.getElementById('navbar').style.transition= '0.5s'
 })
 
 window.onscroll = function() {myFunction()};
