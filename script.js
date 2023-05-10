331867e422b57375ef18af8ca9193d76449e9479
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-05-11 00:27:03 +0530

    smooth side bar

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
+let menuicons = document.getElementById("navbaricons");
+menuicons.addEventListener("click", function () {
+  document.getElementById("navbar").style.transition = "0.5s";
+  document.getElementById("navbar").style.right= "0px";
+  document.body.style.overflow = 'hidden';
+});
 
-let menuicons = document.getElementById('navbaricons');
-menuicons.addEventListener('click' , function(){
-    document.getElementById('navbar').style.transition= '0.5s'
-    document.getElementById('navbar').style.visibility = 'visible'
-})
-let crossicons = document.getElementById('crossicon');
-crossicons.addEventListener('click', function(){
-  document.getElementById('navbar').style.visibility = 'hidden';
-})
+let crossicons = document.getElementById("crossicon");
+crossicons.addEventListener("click", function () {
+  document.getElementById("navbar").style.right = "-100%";
+  document.getElementById("navbar").style.transition = "0.9s";
+  document.body.style.overflow = 'scroll';
+  
+});
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 var header = document.getElementById("header");
 
 function myFunction() {
   if (window.pageYOffset >= 170) {
-    header.classList.add("sticky")
-  } else {
+    document.getElementById("navbar").style.top= "70px";
+    header.classList.add("sticky");
+  
+  } 
+ 
+  else {
     header.classList.remove("sticky");
+    document.getElementById("navbar").style.top= "100px";
+  
   }
 }
+
