f74f9656cdb06b5c5f8a8b943957e2f4086386f3
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-05-11 22:07:23 +0530

    added box shadow to side bar

--- login.js
+++ login.js
@@ -3,12 +3,14 @@ menuicons.addEventListener("click", function () {
   document.getElementById("navbar").style.transition = "0.5s";
   document.getElementById("navbar").style.right= "0px";
   document.body.style.overflow = 'hidden';
+  document.getElementById("navbar").style.boxShadow = "0 0 0 10000px rgba(0,0,0,.50)";
 });
 let crossicons = document.getElementById("crossicon");
 crossicons.addEventListener("click", function () {
   document.getElementById("navbar").style.right = "-100%";
   document.getElementById("navbar").style.transition = "0.9s";
   document.body.style.overflow = 'scroll';
+  document.getElementById("navbar").style.boxShadow = "0 0 0 0px rgba(0,0,0,0)";
 });
 
 window.onscroll = function () {
@@ -20,9 +22,7 @@ var header = document.getElementById("header");
 function myFunction() {
   if (window.pageYOffset >= 170) {
     header.classList.add("sticky");
-    document.getElementById("navbar").style.top= "70px";
   } else {
     header.classList.remove("sticky");
-    document.getElementById("navbar").style.top= "100px";
   }
 }
--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let menuicons = document.getElementById("navbaricons");
 menuicons.addEventListener("click", function () {
   document.getElementById("navbar").style.transition = "0.5s";
   document.getElementById("navbar").style.right= "0px";
+  document.getElementById("navbar").style.boxShadow = "0 0 0 10000px rgba(0,0,0,.50)";
   document.body.style.overflow = 'hidden';
+  
 });
 
 let crossicons = document.getElementById("crossicon");
@@ -10,6 +12,7 @@ crossicons.addEventListener("click", function () {
   document.getElementById("navbar").style.right = "-100%";
   document.getElementById("navbar").style.transition = "0.9s";
   document.body.style.overflow = 'scroll';
+  document.getElementById("navbar").style.boxShadow = "0 0 0 0px rgba(0,0,0,0)";
   
 });
 
@@ -21,14 +24,13 @@ var header = document.getElementById("header");
 
 function myFunction() {
   if (window.pageYOffset >= 170) {
-    document.getElementById("navbar").style.top= "70px";
+    
     header.classList.add("sticky");
   
   } 
  
   else {
     header.classList.remove("sticky");
-    document.getElementById("navbar").style.top= "100px";
   
   }
 }
